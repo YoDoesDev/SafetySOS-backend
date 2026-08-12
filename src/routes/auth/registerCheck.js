cd31f47a30bdc125f674fc56ebccb1a5667dc99c
YoDoesDev <hellowspaa@gmail.com>
2026-08-12 21:51:30 +0530

    update registerCheck.js

---
+++ src/routes/auth/registerCheck.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/auth/register-check", (req, res) => {
+  res.status(200).json({
+    navigateToOtp = true
+  })
+});
