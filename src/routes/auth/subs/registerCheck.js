dcbef081406890f35bb7fe180c2488e499d870b9
YoDoesDev <hellowspaa@gmail.com>
2026-08-13 21:58:51 +0530

    update registerCheck.js

--- src/routes/auth/subs/registerCheck.js
+++ src/routes/auth/subs/registerCheck.js
@@ -2,17 +2,17 @@ const express = require("express");
 const router = express.Router();
 const { getRecord } = require("../../../utils/handlers/data/getRecord.js")
 
-router.post("/auth/register-check", (req, res) => {
+router.post("/auth/register-check", async (req, res) => {
   const { username, phone } = req.body;
   let navigateToOtp = true;
   let reason;
   
-  if(getRecord("accounts", username)){
+  if(await getRecord("accounts", username)){
     navigateToOtp = false;
     reason = "Username already exists."
   }
   
-  if(getRecord("accounts", phone)){
+  if(await getRecord("accounts", phone)){
     navigateToOtp = false;
     reason = "Phone number already exists."
   }
