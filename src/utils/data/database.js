d2f474ab5e1a2fcbdd41d64bb907e95b62f98c04
YoDoesDev <hellowspaa@gmail.com>
2026-08-16 21:07:20 +0530

    update database.js

--- src/utils/data/database.js
+++ src/utils/data/database.js
@@ -1,6 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
 const logger = require("../middleware/logger.js");
 
+require("dotenv").config()
+
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
