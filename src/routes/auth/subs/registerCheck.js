0e85666a4adc64c8411cab54a593431603f0dc14
YoDoesDev <hellowspaa@gmail.com>
2026-08-18 09:13:01 +0530

    update registerCheck.js

--- src/routes/auth/subs/registerCheck.js
+++ src/routes/auth/subs/registerCheck.js
@@ -10,11 +10,21 @@ router.post("/auth/register-check", async (req, res) => {
   if(await getRecord("users", username)){
     navigateToOtp = false;
     reason = "Username already exists."
+    
+    res.status(409).json({
+      navigateToOtp, 
+      reason
+    });
   }
   
   if(await getRecord("users", phone)){
     navigateToOtp = false;
     reason = "Phone number already exists."
+    
+    res.status(409).json({
+      navigateToOtp, 
+      reason
+    });
   }
   
   res.status(200).json({
