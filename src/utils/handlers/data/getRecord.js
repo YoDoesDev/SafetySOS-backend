374853c54ad29fac6368aecdf0537fedbec8f289
YoDoesDev <hellowspaa@gmail.com>
2026-08-18 10:01:18 +0530

    update getRecord.js

--- src/utils/handlers/data/getRecord.js
+++ src/utils/handlers/data/getRecord.js
@@ -46,6 +46,7 @@ const getRecord = async (tableName, val1, val2 = null, customColumn = null) => {
     logger.error(`[REDIS WRITE ERROR] ${err.message}`);
   }
 
+  logger.message(data);
   return data;
 };
 
