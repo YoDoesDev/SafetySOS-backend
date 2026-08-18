4076429fc4a0431b95807888612dee3c543faf38
YoDoesDev <hellowspaa@gmail.com>
2026-08-18 09:54:42 +0530

    update getRecord.js

--- src/utils/handlers/data/getRecord.js
+++ src/utils/handlers/data/getRecord.js
@@ -32,6 +32,7 @@ const getRecord = async (tableName, val1, val2 = null, customColumn = null) => {
 
   if (error) {
     logger.error(`[SUPABASE SELECT ERROR] ${error.message}`);
+    logger.error(data);
     return null;
   }
 
