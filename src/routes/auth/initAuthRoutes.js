077bd263f6cc6191d0adc4971750a8c87fedf62f
YoDoesDev <hellowspaa@gmail.com>
2026-09-13 11:37:32 +0530

    update initAuthRoutes.js

--- src/routes/auth/initAuthRoutes.js
+++ src/routes/auth/initAuthRoutes.js
@@ -2,12 +2,14 @@ const regCheckRoute = require("./subs/registerCheck.js");
 const registerRoute = require("./subs/register.js");
 const loginRoute = require("./subs/login.js");
 const phoneCheckRoute = require("./subs/phoneCheck.js");
+const forgotPasswordLoginRoute = require("./subs/forgotPasswordLogin.js");
 
 const initAuthRoutes = (app) => {
   app.use("", regCheckRoute);
   app.use("", registerRoute);
   app.use("", loginRoute);
-  app.use("", phoneCheckRoute)
+  app.use("", phoneCheckRoute);
+  app.use("", forgotPasswordLoginRoute);
 }
 
 module.exports = initAuthRoutes;
